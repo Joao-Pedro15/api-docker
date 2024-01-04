adeee46d4f41c03532c4e234bb0f20903ce2fa0b
João Pedro <jjoao.monteiro15@gmail.com>
2024-01-04 14:43:52 -0300

    feat: implements mysql database in docker-compose and connect your application

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,13 @@
 FROM node:16
 WORKDIR /server
 
+COPY package.json .
+RUN npm i 
+COPY tsconfig.json .
+
 ENV DOCKERIZE_VERSION v0.7.0
 
 RUN apt-get update \
  && apt-get install -y wget \
  && wget -O - https://github.com/jwilder/dockerize/releases/download/$DOCKERIZE_VERSION/dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz | tar xzf - -C /usr/local/bin \
  && apt-get autoremove -yqq --purge wget && rm -rf /var/lib/apt/lists/*
-
-COPY package.json .
-RUN npm i 
-COPY tsconfig.json .
-EXPOSE 3333
-CMD [ "npm", "run", "dev" ]
--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,23 @@
 import 'dotenv/config'
 import express from 'express'
+import { PoolConfig, createConnection } from 'mysql'
 
 const app = express()
 const PORT = process.env.PORT_APP || 3333
 
+const config: PoolConfig = {
+ host: 'db',
+ user: 'root',
+ password: 'root',
+ database: 'nodedb'
+}
+
+const connection = createConnection(config)
+connection.end()
+
 app.get('/health', (_, res) => { res.send('Hello World!') })
 
+app.get('/novarota', (_, res) => res.send("opa"))
 
 app.listen(PORT, () => {
  console.log("server running on port: " + PORT)
