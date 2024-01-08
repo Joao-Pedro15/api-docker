64edb67c6fcb2ab71d193f8cc4d961c843b27197
João Pedro <jjoao.monteiro15@gmail.com>
2024-01-08 18:07:04 -0300

    feat: public properties in userEntity

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -2,12 +2,12 @@ import { randomUUID } from "crypto";
 import { UserDTO } from "./dtos/UserDTO";
 
 export class User {
- private readonly id: string
- private readonly name: string
- private readonly email: string
- private readonly password: string | null
- private readonly photo: string | null
- private readonly googleID: string | null
+ public readonly id: string
+ public readonly name: string
+ public readonly email: string
+ public readonly password: string | null
+ public readonly photo: string | null
+ public readonly googleID: string | null
 
  constructor({ id, email, googleID, name, password, photo }: UserDTO) {
   this.id = id ?? randomUUID()
