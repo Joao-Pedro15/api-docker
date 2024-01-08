419de8c4a38a492e58a8c44a88d592bf6a4e5165
João Pedro <jjoao.monteiro15@gmail.com>
2024-01-08 19:09:30 -0300

    feat: implements controller and finished get all users

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,8 @@
 import 'dotenv/config'
 import './config/module-alias'
 import express from 'express'
-import { Connection, PoolConfig, QueryFunction, createConnection } from 'mysql'
-import { UserMySqlRepository } from './repositories/implementation/UserMySqlRepository'
-import { promisify } from 'util'
+import { makeLoadUserControllerFactory } from './controllers/user/loadAll/LoadUserControllerFactory'
+import { adaptRoute } from './main/route'
 
 const app = express()
 
@@ -11,29 +10,12 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 const PORT = process.env.PORT_APP || 3333
 
-const config: PoolConfig = {
- host: 'db',
- user: 'root',
- password: 'root',
- database: 'nodedb'
-}
-
-const connection = createConnection(config)
 
 app.get('/health', (_, res) => { res.send('Hello World!') })
 
 app.get('/novarota', (_, res) => res.send("opa"))
 
-app.get('/getAll', async (req, res) => {
- try {
-  const repository = new UserMySqlRepository(connection)
-  const user = await repository.find()
-  connection.end()
-  return res.send(user)
- } catch (error) {
-  return res.send(error)
- }
-})
+app.get('/getAll', adaptRoute(makeLoadUserControllerFactory()))
 
 app.listen(PORT, () => {
  console.log("server running on port: " + PORT)
