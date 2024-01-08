edcf6aabb97a6a668b7aa2ff900e8d517ef6e02e
João Pedro <jjoao.monteiro15@gmail.com>
2024-01-08 20:14:02 -0300

    feat: implements redis in controller loadUser

--- src/controllers/user/loadAll/LoadUserController.ts
+++ src/controllers/user/loadAll/LoadUserController.ts
@@ -1,16 +1,21 @@
+import { RedisCache } from "@/infra/redis";
 import { Controller } from "@/main/controller";
 import { ok } from "@/main/http";
 import { UserServices } from "@/services/user/UserServices";
 
 export class LoadUserController extends Controller {
  constructor(
-  private userService: UserServices
+  private userService: UserServices,
+  private redisCache: RedisCache
  ) {
   super()
  }
 
  async execute() {
+  const cache = await this.redisCache.get('all')
+  if (cache) return ok({ users: cache })
   const users = await this.userService.find()
+  await this.redisCache.set('all', JSON.stringify(users))
   return ok({ users })
 
  }
--- src/controllers/user/loadAll/LoadUserControllerFactory.ts
+++ src/controllers/user/loadAll/LoadUserControllerFactory.ts
@@ -1,6 +1,7 @@
+import { RedisCache } from '@/infra/redis'
 import { LoadUserController } from './LoadUserController'
 import { makeUserServiceFactory } from '@/services/user/UserServicesFactory'
 
 export const makeLoadUserControllerFactory = (): LoadUserController => {
- return new LoadUserController(makeUserServiceFactory())
+ return new LoadUserController(makeUserServiceFactory(), new RedisCache())
 }
