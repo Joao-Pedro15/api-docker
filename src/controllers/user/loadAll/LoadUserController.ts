0ff3ea4a91bdc281c13552ca5d8c5dfdaf548aff
João Pedro <jjoao.monteiro15@gmail.com>
2024-01-09 20:27:12 -0300

    feat: implements rabbitmq in getAll route for example

--- src/controllers/user/loadAll/LoadUserController.ts
+++ src/controllers/user/loadAll/LoadUserController.ts
@@ -1,3 +1,4 @@
+import { RabbitMQ } from "@/infra/rabbitmq";
 import { RedisCache } from "@/infra/redis";
 import { Controller } from "@/main/controller";
 import { ok } from "@/main/http";
@@ -6,13 +7,16 @@ import { UserServices } from "@/services/user/UserServices";
 export class LoadUserController extends Controller {
  constructor(
   private userService: UserServices,
-  private redisCache: RedisCache
+  private redisCache: RedisCache,
+  private rabbitMQServer: RabbitMQ
  ) {
   super()
  }
 
  async execute() {
   const cache = await this.redisCache.get('all')
+  await this.rabbitMQServer.start()
+  await this.rabbitMQServer.publishInQueue('express', JSON.stringify({ message: 'hello' }))
   if (cache) return ok({ users: cache })
   const users = await this.userService.find()
   await this.redisCache.set('all', JSON.stringify(users))
--- src/controllers/user/loadAll/LoadUserControllerFactory.ts
+++ src/controllers/user/loadAll/LoadUserControllerFactory.ts
@@ -1,7 +1,12 @@
 import { RedisCache } from '@/infra/redis'
 import { LoadUserController } from './LoadUserController'
 import { makeUserServiceFactory } from '@/services/user/UserServicesFactory'
+import { RabbitMQ } from '@/infra/rabbitmq'
 
 export const makeLoadUserControllerFactory = (): LoadUserController => {
- return new LoadUserController(makeUserServiceFactory(), new RedisCache())
-}
+ return new LoadUserController(
+  makeUserServiceFactory(),
+  new RedisCache(),
+  new RabbitMQ()
+ )
+}  
