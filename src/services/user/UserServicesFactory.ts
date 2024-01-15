e202e07cdcb7b74536c6d1a99be1cf2a2b0d6609
João Pedro <jjoao.monteiro15@gmail.com>
2024-01-15 11:13:36 -0300

    feat: create logical in UserService call in elasticsearch or in mysql

--- src/services/user/UserServicesFactory.ts
+++ src/services/user/UserServicesFactory.ts
@@ -1,8 +1,16 @@
 import { Database } from "@/infra/database"
 import { UserServices } from "./UserServices"
 import { UserMySqlRepository } from "@/repositories/implementation/UserMySqlRepository"
+import { ElasticSearch } from "@/infra/elasticsearch"
+import { UserElasticRepository } from "@/repositories/implementation/UserElasticRepository"
 
-export const makeUserServiceFactory = () => {
+export const makeUserServiceFactory = (isElastic = false) => {
+ if (isElastic) {
+  const database = new ElasticSearch().instance
+  const userRepository = new UserElasticRepository(database)
+  return new UserServices(userRepository)
+ }
+
  const database = new Database().connection
  const userRepository = new UserMySqlRepository(database)
  return new UserServices(userRepository)
