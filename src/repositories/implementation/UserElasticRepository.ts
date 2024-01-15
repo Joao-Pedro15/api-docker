d6ab1bae83880118d76a657dfbd7270924ac5d53
João Pedro <jjoao.monteiro15@gmail.com>
2024-01-15 11:12:40 -0300

    feat: create elasticsearch implementation

---
+++ src/repositories/implementation/UserElasticRepository.ts
@@ -0,0 +1,25 @@
+import { Client } from "elasticsearch";
+import { UserRepository } from "../UserRepository";
+import { User } from "@/entities/User";
+
+export class UserElasticRepository implements UserRepository {
+ private readonly db: Client
+ constructor(db: Client) {
+  this.db = db
+ }
+
+ async find(): Promise<User[]> {
+  const query = await this.db.search({
+   index: 'users',
+   size: 100
+  })
+  console.log(query)
+  const users = query?.hits?.hits?.map(item => item?._source) ?? []
+  return users as User[]
+ }
+
+ async findByEmail(email: string): Promise<User[]> {
+  return []
+ }
+
+}
